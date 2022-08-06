923469be8b428fcf75d9e290d6852928212cd8e6
Trần Thủ Đô <84132830+do2882001@users.noreply.github.com>
2022-08-06 20:38:51 +0700

    Add project files.

---
+++ eProject3_NGO_Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eProject3_NGO_Website.Startup))]
+namespace eProject3_NGO_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
